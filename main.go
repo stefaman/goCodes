cf58b222b23c6cad1974b8219e83845cc9b34b07
stefaman <stefaman@qq.com>
2017-06-16 16:03:33 +0800

    fix trailer

--- main.go
+++ main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"reflect"
+	"time"
 )
 
 func main() {
-	// var t  testing.T
-	stdout := reflect.ValueOf(os.Stdout).Elem() // *os.Stdout, an os.File var
-	fmt.Println(stdout.Type())                  // "os.File"
-	fmt.Println(stdout, stdout.CanAddr(), stdout.CanSet(), stdout.CanInterface())
-	stdout.Set(reflect.ValueOf(*os.Stderr))
-	stdout.Set(reflect.ValueOf(*os.Stdin)) //设置为*os.Stdin test无法输出???
-	fd := stdout.FieldByName("fd")
-	fmt.Println(fd.CanAddr(), fd.CanSet(), fd.CanInterface()) //true false false
-	fmt.Println(fd.Kind() == reflect.Uintptr, fd.Uint())      //windows:ture, XXX
-	// fmt.Println(fd.Kind() == reflect.Int, fd.Int()) //linux:true, 1
-	// fd.SetInt(2) // panic: using value obtained using unexported field
-	// fmt.Println(fd.Interface()) //cannot return value obtained from unexported field or method
+
+	fmt.Println(time.Now().Add(120*time.Second).UTC().Format("Mon, 02 Jan 2006 15:04:05 GMT"))
+fmt.Println(time.Now().Add(120*time.Second).UTC().Format(time.RFC1123))
 }
--- stef/trailer/server.go
+++ stef/trailer/server.go
@@ -35,27 +35,30 @@ func ContentHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeContent(w, r, path, mtime, file)
 }
 
+//TrailerHandler add trailer headers.
+//output see "out.txt", curl -i --raw , explorer's dev tool cannot display trailers
 func TrailerHandler(w http.ResponseWriter, req *http.Request) {
 	// Before any call to WriteHeader or Write, declare
 	// the trailers you will set during the HTTP
 	// response. These three headers are actually sent in
 	// the trailer.
 	w.Header().Set("Trailer", "AtEnd1, AtEnd2")
-	w.Header().Add("Trailer", "AtEnd3")
+	w.Header().Add("Trailer", "Expires")
+	w.Header().Set(http.TrailerPrefix+"atfirst", "dd")
 
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
 	w.WriteHeader(http.StatusOK)
-
 	w.Header().Set("AtEnd1", "value 1")
-	io.WriteString(w, "This HTTP response has both headers before this text and trailers at the end.\n")
-	// if flusher, ok := w.(http.Flusher); ok {
-	// 	log.Println("flush")
-	// 	flusher.Flush()
-	// }
-	time.Sleep(1 * time.Second)
-	io.WriteString(w, "AtEnd1: valu1\n")
-	w.Header().Set("AtEnd2", "value 2")
-	w.Header().Set("AtEnd3", "value 3") // These will appear as trailers.
 
-	w.Header().Set(http.TrailerPrefix+"kk", "dd")
+	io.WriteString(w, "This HTTP response has both headers before this text and trailers at the end.\n")
+	if flusher, ok := w.(http.Flusher); ok {
+		flusher.Flush()
+	}
+	time.Sleep(1 * time.Second)
+	io.WriteString(w, "just another string\n")
+	w.Header().Set("AtEnd2", "value 2")
+	w.Header().Set("Expires", time.Now().Add(120*time.Second).UTC().Format("Mon, 02 Jan 2006 15:04:05 GMT")) // These will appear as trailers.
+
+	w.Header().Set(http.TrailerPrefix+"atlast", "dd")
+	w.Header().Set("lost", "error")//not work
 }
